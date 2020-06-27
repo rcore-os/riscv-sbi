7ce0df03601cf9e92c567443a3d1b700c1ff47e5
luojia65 <me@luojia.cc>
2020-06-27 11:35:50 +0800

    Move from riscv-sbi-rt project

---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+//! Low level access to RISC-V Supervisor Binary Interface (SBI) implementations
+//!
+//! Ref: https://github.com/riscv/riscv-sbi-doc/blob/master/riscv-sbi.adoc
+//!
+//! 
+//! # Features
+//! 
+//! This crate provides access to standard SBI functions, both base and legacy ones.
+
+#![no_std]
+#![deny(warnings, missing_docs)]
+#![feature(llvm_asm)]
+
+pub mod base;
+pub mod legacy;
+
+pub mod log;
+
+#[macro_use]
+#[doc(hidden)]
+pub mod io;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct SbiReturn {
+    error: SbiError,
+    value: usize,
+}
+
+impl SbiReturn {
+    fn unwrap(self) -> usize {
+        assert_eq!(self.error, SbiError::Success);
+        self.value
+    }
+}
+
+/// The error type which is returned from SBI.
+#[repr(isize)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[allow(missing_docs)]
+pub enum SbiError {
+    Success = 0,
+    Failed = -1,
+    NotSupported = -2,
+    InvalidParam = -3,
+    Denied = -4,
+    InvalidAddress = -5,
+    AlreadyAvailable = -6,
+}
+
+/// The type returned by SBI functions.
+pub type SbiResult<T = ()> = Result<T, SbiError>;
+
+impl From<SbiReturn> for SbiResult<usize> {
+    fn from(ret: SbiReturn) -> Self {
+        match ret.error {
+            SbiError::Success => Ok(ret.value),
+            err => Err(err),
+        }
+    }
+}
+
+#[inline(always)]
+fn sbi_call(ext_id: usize, func_id: usize, arg0: usize, arg1: usize, arg2: usize) -> SbiReturn {
+    let error;
+    let value;
+    unsafe {
+        llvm_asm!(
+            "ecall"
+            : "={x10}" (error), "={x11}"(value)
+            : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x16}"(func_id), "{x17}" (ext_id)
+            : "memory"
+            : "volatile"
+        );
+    }
+    SbiReturn { error, value }
+}
